99d8be1cdd7481ea28cc63d95c494142901fbd86
Artem <artem.matseruk@gmail.com>
2024-09-30 14:53:21 +0300

    update: hided empty categories

--- src/utils/axios/getCategories.ts
+++ src/utils/axios/getCategories.ts
@@ -3,6 +3,6 @@ import { WORDPRESS_API_PATHS } from '@/config/constants'
 
 export const getCategories = async () => {
   return await axios.get(
-    `${process.env.WORDPRESS_API_URL!}/${WORDPRESS_API_PATHS.fetch}/categories`,
+    `${process.env.WORDPRESS_API_URL!}/${WORDPRESS_API_PATHS.fetch}/categories?hide_empty=true`,
   )
 }
