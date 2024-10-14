a0768c06cd90ad3751e40eeb67fe74c3f1177b1d
Artem <artem.matseruk@gmail.com>
2024-10-14 12:09:28 +0300

    update: added link to admin dashboard on email

--- src/utils/email/bowserSendEmail.ts
+++ src/utils/email/bowserSendEmail.ts
@@ -1,5 +1,5 @@
-import { ISendEmail } from '@/utils/email/index'
 import axios from 'axios'
+import { ISendEmail } from '@/utils/email/index'
 
 export const browserSendEmail = async ({
   subject,
@@ -8,7 +8,7 @@ export const browserSendEmail = async ({
   htmlMessage = `
     <h3 style="font-family: Arial, Helvetica;">Hello!</h3>
     <p style="font-family: Arial, Helvetica;">A new request has just been submitted through the website and has been successfully saved in the admin panel.</p>
-    <p style="font-family: Arial, Helvetica;">Please log in to the admin dashboard to review the details.</p>
+    <p style="font-family: Arial, Helvetica;">Please log in to the <a href="https://admin.luminarcapital.com/wp-admin/" target="_blank">admin dashboard</a> to review the details.</p>
     <p style="font-family: Arial, Helvetica;">Thank you!</p>
     <p style="font-family: Arial, Helvetica; margin-bottom: 0;">Best regards,</p>
     <p style="font-family: Arial, Helvetica; margin-top: 0;">Luminar Capital Team</p>
