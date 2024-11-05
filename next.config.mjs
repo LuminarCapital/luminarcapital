7a437f05d802de5fdff6162bfc48479128b58005
Artem <artem.matseruk@gmail.com>
2024-11-05 10:46:23 +0200

    bugfix: set noindex for headers

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,21 @@ const nextConfig = {
   },
   env: {
     WORDPRESS_API_URL: process.env.WORDPRESS_API_URL
+  },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
   }
 };
 
