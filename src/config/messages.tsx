ca7b8d8f8e8fb3efe3bc65d8191ea903d8af7c9b
Artem <artem.matseruk@gmail.com>
2024-12-05 15:17:29 +0200

    update: changed email's text size, and space

--- src/config/messages.tsx
+++ src/config/messages.tsx
@@ -32,20 +32,20 @@ export const messages: { [key: string]: MessageValue } = {
     </ul>
     <p style="font-family: Arial, Helvetica, sans-serif;">Please log in to the <a href="https://admin.luminarcapital.com/wp-admin/" target="_blank">admin dashboard</a> to review the details.</p>
     <p style="font-family: Arial, Helvetica, sans-serif;">Thank you!</p>
-    <p style="font-family: Arial, Helvetica, sans-serif; margin-bottom: 0;">Best regards,</p>
-    <p style="font-family: Arial, Helvetica, sans-serif; margin-top: 0;">Luminar Capital Team</p>
+    <p style="font-family: Arial, Helvetica, sans-serif;">Best regards,</p>
+    <p style="font-family: Arial, Helvetica, sans-serif;"><b>Luminar Capital Team</b></p>
 `
   },
   user: () => {
     return `
-  <h3 style="font-family: Arial, Helvetica, sans-serif; font-size: 13px;">Hello!</h3>
-  <p style="font-family: Arial, Helvetica, sans-serif; font-size: 11px;">Thank you for completing the form on our website.</p>
-  <p style="font-family: Arial, Helvetica, sans-serif; margin-top: 0; font-size: 11px;">We have successfully received your inquiry and our team will review it shortly.</p>
+  <h3 style="font-family: Arial, Helvetica, sans-serif;">Hello!</h3>
+  <p style="font-family: Arial, Helvetica, sans-serif;">Thank you for completing the form on our website.</p>
+  <p style="font-family: Arial, Helvetica, sans-serif;">We have successfully received your inquiry and our team will review it shortly.</p>
  
-  <p style="font-family: Arial, Helvetica, sans-serif; margin-top: 0; font-size: 11px;">One of our specialists will get in touch with you within 1-2 business days to discuss your request in more detail and provide any necessary information.</p>
-  <p style="font-family: Arial, Helvetica, sans-serif; font-size: 11px;">We truly appreciate your interest in our services, and we look forward to assisting you!</p>
-  <p style="font-family: Arial, Helvetica, sans-serif; margin-bottom: 0; font-size: 11px;">Best regards,</p>
-  <p style="font-family: Arial, Helvetica, sans-serif; margin-top: 0; font-size: 11px;"><b>Luminar Capital Team</b></p>
+  <p style="font-family: Arial, Helvetica, sans-serif;">One of our specialists will get in touch with you within 1-2 business days to discuss your request in more detail and provide any necessary information.</p>
+  <p style="font-family: Arial, Helvetica, sans-serif; ">We truly appreciate your interest in our services, and we look forward to assisting you!</p>
+  <p style="font-family: Arial, Helvetica, sans-serif;">Best regards,</p>
+  <p style="font-family: Arial, Helvetica, sans-serif;"><b>Luminar Capital Team</b></p>
   `
   },
 }
